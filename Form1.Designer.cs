db8e37151bc4ff28dd95a0dbb4afff657ef39afc
0_305_11 <0_305_11@1_305_11>
2023-12-05 18:26:27 +0500

    жестко рабоать

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnForm = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.txtStep = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(20, 17);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(650, 335);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btnForm
+            // 
+            this.btnForm.Location = new System.Drawing.Point(12, 372);
+            this.btnForm.Name = "btnForm";
+            this.btnForm.Size = new System.Drawing.Size(84, 33);
+            this.btnForm.TabIndex = 1;
+            this.btnForm.Text = "Форма2";
+            this.btnForm.UseVisualStyleBackColor = true;
+            this.btnForm.Click += new System.EventHandler(this.btnForm_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(573, 375);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(96, 32);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Построить";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(152, 379);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(39, 20);
+            this.txtX.TabIndex = 3;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(197, 379);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(39, 20);
+            this.txtA.TabIndex = 3;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(242, 379);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(39, 20);
+            this.txtB.TabIndex = 3;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(287, 379);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(39, 20);
+            this.txtC.TabIndex = 3;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(332, 379);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(39, 20);
+            this.txtMin.TabIndex = 3;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(377, 379);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(39, 20);
+            this.txtMax.TabIndex = 3;
+            // 
+            // txtStep
+            // 
+            this.txtStep.Location = new System.Drawing.Point(422, 379);
+            this.txtStep.Name = "txtStep";
+            this.txtStep.Size = new System.Drawing.Size(39, 20);
+            this.txtStep.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "a";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(256, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "b";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(303, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "c";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(341, 363);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Xmin";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(383, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Xmax";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(432, 363);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Step";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(696, 430);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStep);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.btnForm);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Графики функций";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnForm;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.TextBox txtStep;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
