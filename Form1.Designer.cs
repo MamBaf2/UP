6fd2b1c2529e30b34ea23c806bb60b34def94198
MamBaf3 <artem-stikhin@mail.ru>
2023-12-23 13:06:57 +0500

    интерфейс

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(754, 262);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(687, 348);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(92, 45);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // btnDel
+            // 
+            this.btnDel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDel.Location = new System.Drawing.Point(265, 348);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(92, 45);
+            this.btnDel.TabIndex = 1;
+            this.btnDel.Text = "Удалить запись";
+            this.btnDel.UseVisualStyleBackColor = false;
+            // 
+            // btnChange
+            // 
+            this.btnChange.BackColor = System.Drawing.Color.White;
+            this.btnChange.Location = new System.Drawing.Point(151, 348);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(92, 45);
+            this.btnChange.TabIndex = 1;
+            this.btnChange.Text = "Изменить запись";
+            this.btnChange.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(36, 348);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 45);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Добавить запись";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSave.Location = new System.Drawing.Point(482, 348);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(92, 45);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Сохранить отчёт";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 427);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(826, 508);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Расписание для спорткомплекса";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,39 @@ namespace УП
         public Form1()
         {
             InitializeComponent();
+            // Создаем и добавляем столбцы в DataGridView
+            dataGridView1.ColumnCount = 7;
+            dataGridView1.Columns[0].Name = "Понедельник";
+            dataGridView1.Columns[1].Name = "Вторник";
+            dataGridView1.Columns[2].Name = "Среда";
+            dataGridView1.Columns[3].Name = "Четверг";
+            dataGridView1.Columns[4].Name = "Пятница";
+            dataGridView1.Columns[5].Name = "Суббота";
+            dataGridView1.Columns[6].Name = "Воскресенье";
+
+            // Добавляем строки для каждого часа в каждом дне недели
+            for (int i = 0; i < 24; i++)
+            {
+                string[] row = new string[7]; // Создаем массив для каждого часа в день
+                for (int j = 0; j < 7; j++)
+                {
+                    row[j] = ""; // По умолчанию пустые значения для каждого часа
+                }
+                dataGridView1.Rows.Add(row);
+            }
+
+            /* Пример заполнения расписания (замените на ваши данные)
+            dataGridView1.Rows[8].Cells[0].Value = "Футбол"; // Футбол в 8:00 на Понедельник
+            dataGridView1.Rows[10].Cells[1].Value = "Плавание"; // Плавание в 10:00 на Вторник
+            И так далее... */
+
+            // Устанавливаем высоту строк для лучшего отображения данных
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
-}
+    }
